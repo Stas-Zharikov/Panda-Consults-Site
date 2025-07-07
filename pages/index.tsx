2ec53b2126de76cd15c89837e3ce6f6c9391d1e0
Stas-Zharikov <stas.zharikov.j@gmail.com>
2025-07-07 11:37:41 +0300

    Delete pages/index.tsx

--- pages/index.tsx
+++
@@ -1,7 +0,0 @@
-import dynamic from "next/dynamic";
-
-const WhispersPrototype = dynamic(() => import("../components/WhispersPrototype"), { ssr: false });
-
-export default function Home() {
-  return <WhispersPrototype />;
-}
